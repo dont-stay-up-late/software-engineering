b70efa50473c816330b0d61e57278fad2ea6ab68
waley22 <waley22@163.com>
2019-10-14 21:21:12 +0800

    Added additional attacker and defender classes

--- src/models.py
+++ src/models.py
@@ -45,6 +45,10 @@ class Model(Sprite, metaclass=abc.ABCMeta):
     def attacked(self, loss):
         pass
 
+    def getCoordinate(self):
+        coord = int(self.position[0] / 100), int(self.position[1] / 100)
+        # 100 is used for spacefilling; the true value should be the length of a tile on the map
+        # Returns a tuple for the coordinate of the character
 
 class Defender(Model):
     '''
@@ -55,6 +59,8 @@ class Defender(Model):
     def __init__(self, position, direction):
         super().__init__(position, direction)
         Defender.defenders.append(self)
+        # All defenders cannot move
+        self.speed = 0
     
     @abc.abstractmethod
     def special(self):
@@ -75,6 +81,11 @@ class Attacker(Model):
     def die(self):
         pass
 
+    # All attackers may move
+    def move(self):
+        self.position[0] += Model.MOVEMENT[self.direction][0] * self.speed
+        self.position[1] += Model.MOVEMENT[self.direction][1] * self.speed
+
 
 # The following are defenders.
 
@@ -90,7 +101,6 @@ class CivilianDefender(Defender):
         self.attack_time = 2
         self.defend_power = 0
         self.hp = 100
-        self.speed = 0
         self.cost = 10
         self.cool_down_time = 15
         self.init_image()
@@ -99,6 +109,166 @@ class CivilianDefender(Defender):
         # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
         pass
     
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class FattyDefender(Defender):
+    '''
+    Fatty defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 5
+        self.attack_time = 2
+        self.defend_power = 0
+        self.hp = 400
+        self.cost = 18
+        self.cool_down_time = 25
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class KamikazeDefender(Defender):
+    '''
+    Kamikaze defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 20
+        self.attack_time = 1
+        self.defend_power = 0
+        self.hp = 40
+        self.cost = 10
+        self.cool_down_time = 10
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class PharmacistDefender(Defender):
+    '''
+    Pharmacist defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 0
+        self.attack_time = None
+        self.defend_power = 0
+        self.hp = 50
+        self.cost = 25
+        self.cool_down_time = 80
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class AuraDefender(Defender):
+    '''
+    Aura defender class who provides buffs.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 0
+        self.attack_time = None
+        self.defend_power = 0
+        self.hp = 90
+        self.cost = 20
+        self.cool_down_time = 60
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class BombDefender(Defender):
+    '''
+    Bomb defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 0
+        self.attack_time = None
+        self.defend_power = 0
+        self.hp = 200
+        self.cost = 28
+        self.cool_down_time = 120
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
     def update(self):
         pass
 
@@ -146,6 +316,167 @@ class CivilianAttacker(Attacker):
     def attacked(self, loss):
         pass
 
-    def move(self):
-        self.position[0] += Model.MOVEMENT[self.direction][0] * self.speed
-        self.position[1] += Model.MOVEMENT[self.direction][1] * self.speed
+class FattyAttacker(Attacker):
+    '''
+    Fatty defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 25
+        self.attack_time = 4
+        self.defend_power = 0
+        self.hp = 400
+        self.speed = 0.1
+        self.cost = 18
+        self.cool_down_time = 25
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class KamikazeAttacker(Attacker):
+    '''
+    Kamikaze defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 50
+        self.attack_time = 0.5
+        self.defend_power = 0
+        self.hp = 40
+        self.speed = 0.4
+        self.cost = 15
+        self.cool_down_time = 10
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class PharmacistAttacker(Attacker):
+    '''
+    Pharmacist defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 10
+        self.attack_time = 1
+        self.defend_power = 0
+        self.hp = 150
+        self.speed = 0.2
+        self.cost = 25
+        self.cool_down_time = 80
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class AuraAttacker(Attacker):
+    '''
+    Aura defender class who provides buffs.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 30
+        self.attack_time = 1.5
+        self.defend_power = 0
+        self.hp = 240
+        self.speed = 0.25
+        self.cost = 20
+        self.cool_down_time = 30
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
+
+class BombAttacker(Attacker):
+    '''
+    Bomb defender class.
+    '''
+    last_created_time = 0
+
+    def __init__(self, position, direction):
+        super().__init__(position, direction)
+        self.attack_power = 0
+        self.attack_time = 1
+        self.defend_power = 0
+        self.hp = 210
+        self.speed = 0.2
+        self.cost = 28
+        self.cool_down_time = 90
+        self.init_image()
+
+    def init_image(self):
+        # Prepare UI-related stuff. Should we draw it right in this method (or in next update)?
+        pass
+
+    def update(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def special(self):
+        pass
+
+    def attacked(self, loss):
+        pass
